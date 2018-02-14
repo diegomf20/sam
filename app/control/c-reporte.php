8e07a4f69fe3512efa21320595e4288d2c3d0d4b
palominovega <paloominounprg@gmail.com>
2018-02-14 00:34:54 -0500

    reportes

---
+++ app/control/c-reporte.php
@@ -0,0 +1,44 @@
+<?php
+
+  if (isset($_REQUEST['operacion'])) {
+    /**
+     * includes
+     */
+     include '../db.php';
+     include '../sql/s-reporte.php';
+
+     $sreportes=new sreportes();
+     $operacion=$_REQUEST['operacion'];
+     switch ($operacion) {
+       case 'listaAfiliadosRango':
+         try {
+           $datos=$sreportes->listAfilidosRango();
+           echo json_encode($datos);
+         } catch (Exception $e) {
+           echo $e->getMessage();
+         }
+         break
+
+       case 'cantRango':
+         try {
+           $datos=$sreportes->cantidadRango();
+           echo json_encode($datos);
+         } catch (Exception $e) {
+           echo $e->getMessage();
+         }
+         break;
+
+       case 'faltapaquete':
+         try {
+           $datos=$sreportes->faltapaquete();
+           echo json_encode($datos);
+         } catch (Exception $e) {
+           echo $e->getMessage();
+         }
+         break;
+
+
+     }
+   }
+
+ ?>
---
+++ app/sql/s-reporte.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ *
+ */
+class sreportes
+{
+
+  function listAfilidosRango(){
+    $db=new baseDatos();
+    try {
+      $conexion=$db->conectar();
+      $sql='SELECT  concat(nombres, apellidos) nombre, celular, CASE rango WHEN 0 THEN 'INVERSIONISTA'   WHEN 1 THEN 'EJECUTIVO'  WHEN 2 THEN 'PLATINO'   WHEN 3 THEN 'MASTER' ELSE 'ELITE' END as rango1 '.
+          'FROM tb_inversionista ';
+      $sentencia=$conexion->prepare($sql);
+      $sentencia->execute();
+      $resultados=$sentencia->fetchAll(PDO::FETCH_ASSOC);
+      return $resultados;
+    } catch (PDOException $e) {
+      throw $e->getMessage();
+    }
+  }
+
+  function cantidadRango(){
+    $db=new baseDatos();
+    try {
+      $conexion=$db->conectar();
+      $sql='SELECT COUNT(idinversionista) cant, CASE rango WHEN 0 THEN 'INVERSIONISTA'   WHEN 1 THEN 'EJECUTIVO'  WHEN 2 THEN 'PLATINO'   WHEN 3 THEN 'MASTER' ELSE 'ELITE' END as rango1 '.
+          'FROM tb_inversionista GROUP by rango ';
+      $sentencia=$conexion->prepare($sql);
+      $sentencia->execute();
+      $resultados=$sentencia->fetchAll(PDO::FETCH_ASSOC);
+      return $resultados;
+    } catch (PDOException $e) {
+      throw $e->getMessage();
+    }
+  }
+
+  function faltapaquete(){
+    $db=new baseDatos();
+    try {
+      $conexion=$db->conectar();
+      $sql='SELECT concat(nombres, apellidos) nombre, celular, tb2.fecha from tb_inversionista tb '.
+            'inner join tb_inicial tb2  on tb2.idinversionista=tb.idinversionista '.
+            'left join tb_inversion tb3 on tb3.idinversionista=tb2.idinversionista WHERE tb3.idinversionista is null';
+      $sentencia=$conexion->prepare($sql);
+      $sentencia->execute();
+      $resultados=$sentencia->fetchAll(PDO::FETCH_ASSOC);
+      return $resultados;
+    } catch (PDOException $e) {
+      throw $e->getMessage();
+    }
+  }
+
+  function afilidosRnago(){
+    $db=new baseDatos();
+    try {
+      $conexion=$db->conectar();
+      $sql='';
+      $sentencia=$conexion->prepare($sql);
+      $sentencia->execute();
+      $resultados=$sentencia->fetchAll(PDO::FETCH_ASSOC);
+      return $resultados;
+    } catch (PDOException $e) {
+      throw $e->getMessage();
+    }
+  }
+
+
+
+}
+
+ ?>
